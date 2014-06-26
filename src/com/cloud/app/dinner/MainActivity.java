e3bd2c23ded1b30b4b607686921eb72637e53936
Andrew <jieooo7@163.com>
2014-06-26 15:50:18 +0800

    0626

--- src/com/cloud/app/dinner/MainActivity.java
+++ src/com/cloud/app/dinner/MainActivity.java
@@ -9,22 +9,27 @@
 package com.cloud.app.dinner;
 
 import android.app.Activity;
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 /**
  * com.cloud.app.dinner.MainActivity
+ * 
  * @author Andrew Lee <br/>
- * create at 2014年6月25日 上午9:27:36
+ *         create at 2014年6月25日 上午9:27:36
  */
-@SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
-	
+	private TabHost tabHost;
+	private TabSpec tabSpec;
+
 	/**
 	 * menu按钮，点击按钮展示左侧布局，再点击一次隐藏左侧布局。
 	 */
@@ -34,17 +39,46 @@ public class MainActivity extends Activity {
 	 */
 	private SlidingLayout slidingLayout;
 	private View mView;
-	
-	
-	protected void onCreate(Bundle savedInstanceState){
+
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题栏
 		setContentView(R.layout.main);
+		
+
+		tabHost = (TabHost) findViewById(R.id.tabhost);
+		tabHost.setup();
+
+		tabSpec = tabHost
+				.newTabSpec("one")
+				.setIndicator("one")
+					
+				.setContent(R.id.onetab);
+//						getResources().getDrawable(
+//								R.drawable.tab_selector_coupon))
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost
+				.newTabSpec("two")
+				.setIndicator("one")
+				.setContent(R.id.twotab);
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost
+				.newTabSpec("three")
+				.setIndicator("one")
+				.setContent(R.id.threetab);
+		tabHost.addTab(tabSpec);
+		
+		
+		
+		 tabHost.setCurrentTab(0);
+
 		slidingLayout = (SlidingLayout) findViewById(R.id.slidingLayout);
 		menuButton = (ImageView) findViewById(R.id.left_button);
-		mView=(View) findViewById(R.id.content);
-//		// 将监听滑动事件绑定在contentListView上
-		slidingLayout.setScrollEvent(mView); 
+		mView = (View) findViewById(R.id.content);
+		// // 将监听滑动事件绑定在contentListView上
+		slidingLayout.setScrollEvent(mView);
 		menuButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -56,8 +90,7 @@ public class MainActivity extends Activity {
 				}
 			}
 		});
-		
+
 	}
-	
 
 }
